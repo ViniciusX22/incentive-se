abbcb1ef8575f05cfe7545dc076e63fb78b06ac6
Vinícius X <viniciusxavier2212@gmail.com>
2020-08-18 21:18:06 -0300

    feat: added models to admin interface

--- server/incentiveseapi/incentiveseapi/api/admin.py
+++ server/incentiveseapi/incentiveseapi/api/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Incentive, Request
 
 # Register your models here.
-admin.register(Incentive)
-admin.register(Request)
+
+
+@admin.register(Incentive)
+class IncentiveAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    pass
